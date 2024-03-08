08a39cf3c946b980108bc737cc9a36bf194647d2
Satyajit Samal <samalsatyajit668@gmail.com>
2024-03-08 11:59:56 +0530

    Added  solved-questions/2-conditionals-loops/FibonacciSeries.java and leetcode-questions/2-conditions-loops/SubstractProductAndSumCalculator.java

---
+++ leetcode-questions/2-conditions-loops/SubstractProductAndSumCalculator.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int SubstractProductAndSum(int n) {
+        int sum=0; int prod=1;
+        while(n>0){
+            int rem=n%10;
+            sum=sum+rem;
+            prod=prod*rem;
+            n/=10;
+        }
+        return prod-sum;
+    }
+}
+
+public class SubstractProductAndSumCalculator {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int result = sol.SubstractProductAndSum(234);
+        System.out.println(result);
+    }
+}
