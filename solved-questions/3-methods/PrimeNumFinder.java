bb9aac0d13e556473f659dc097cbd8dcabaeec13
Satyajit Samal <samalsatyajit668@gmail.com>
2024-03-13 19:49:25 +0530

    Modified PrimeNumFinder, Added ThreeDigitArmstrongNum, MinMaxNumberPrinter

--- solved-questions/3-methods/PrimeNumFinder.java
+++ solved-questions/3-methods/PrimeNumFinder.java
@@ -4,18 +4,17 @@ public class PrimeNumFinder {
     public static void main(String[] args) {
         System.out.print("Enter the number: ");
         Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
+        int num = in.nextInt();
         in.close();
-        System.out.println(isPrime(n));
+        System.out.println(isPrime(num));
     }
 
     static boolean isPrime(int n) {
-        if(n<=1)
-            return false;
         int c = 2;
         while(c * c <= n) {
-            if(n % c == 0)
+            if(n % c == 0) {
                 return false;
+            }
             c++;
         }
         return true;
