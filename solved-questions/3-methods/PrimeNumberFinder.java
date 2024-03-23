76e5e21b887e1d4156e3b387fdf9746d4cf22bc0
Satyajit Samal <samalsatyajit668@gmail.com>
2024-03-23 21:34:53 +0530

    Added PrimeNumberFinder.java

---
+++ solved-questions/3-methods/PrimeNumberFinder.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class PrimeNumberFinder {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        int lower = in.nextInt();
+
+        System.out.print("Enter the second number: ");
+        int upper = in.nextInt();
+
+        in.close();
+
+        for(int i = lower; i <= upper; i++) {
+            if(isPrime(i))
+                System.out.print(i + " ");
+        }
+    }
+
+    static boolean isPrime(int n) {
+        if(n == 0 || n == 1) {
+            return false;
+        }
+        if(n == 2) {
+            return true;
+        }
+
+        int i = 2;
+        while(i * i <= n) {
+            if(n % i == 0)
+                return false;
+                i++;
+        }
+        return true;
+
+    }
+}
