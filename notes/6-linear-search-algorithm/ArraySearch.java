02af07c9c11674870b899c2bfa7aaee0efcd5221
Satyajit Samal <samalsatyajit668@gmail.com>
2024-03-26 08:56:36 +0530

    Added new files

---
+++ notes/6-linear-search-algorithm/ArraySearch.java
@@ -0,0 +1,21 @@
+public class ArraySearch {
+    public static void main(String[] args) {
+        int[] nums = {23, 45, 1, 2, 8, 19, -3, 16, -11, 28};
+        int target = 19;
+        int ans = linearSearch(nums, target);
+        System.out.println(ans);
+    }
+
+    static int linearSearch(int[] nums, int target) {
+        if(nums.length == 0)
+            return -1;
+        
+        
+        for(int index = 0; index < nums.length; index++) {
+            int element = nums[index];
+            if(element == target)
+                return index;
+        }
+        return -1;
+    }
+}
