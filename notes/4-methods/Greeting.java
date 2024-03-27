3effd540986a090d6516f9432e219730c62775e2
Satyajit Samal <samalsatyajit668@gmail.com>
2024-03-27 11:29:07 +0530

    Removed some extra files

--- notes/4-methods/Greeting.java
+++
@@ -1,8 +0,0 @@
-public class Greeting {
-    public static void main(String[] args) {
-        greeting();
-    }
-    static void greeting() {
-        System.out.println("Hello World!");
-    }
-}
