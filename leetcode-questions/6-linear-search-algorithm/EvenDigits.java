98dd39e05078a74963bc272cb075bc2a6e90afeb
Satyajit Samal <samalsatyajit668@gmail.com>
2024-03-28 21:41:34 +0530

    Modified a recent file

--- leetcode-questions/6-linear-search-algorithm/EvenDigits.java
+++ leetcode-questions/6-linear-search-algorithm/EvenDigits.java
@@ -1,7 +1,7 @@
-public class EvenDigits {
+public class EvenDigits{
     public static void main(String[] args) {
-        int[] arr = {12, 345, 2, 6, 7896};
-        System.out.println(findNumbers(arr));
+       int[] arr = {12, 345, 2, 6, 7896};
+       System.out.println(findNumbers(arr));
     }
 
     static int findNumbers(int[] arr) {
@@ -14,11 +14,11 @@ public class EvenDigits {
     }
 
     static boolean even(int num) {
-        int numberOfDigits = digits(num);
+        int numberOfDigits = digit(num);
         return numberOfDigits % 2 == 0;
     }
 
-    static int digits(int n) {
+    static int digit(int n) {
         int count = 0;
         while(n > 0) {
             count++;
