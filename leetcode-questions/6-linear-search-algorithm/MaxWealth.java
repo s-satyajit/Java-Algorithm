6645ff6c25497c1bd373bb1e6870c16741f4ce27
Satyajit Samal <samalsatyajit668@gmail.com>
2024-03-29 00:41:57 +0530

    Added new files

--- leetcode-questions/6-linear-search-algorithm/MaxWealth.java
+++ leetcode-questions/6-linear-search-algorithm/MaxWealth.java
@@ -2,22 +2,20 @@ public class MaxWealth {
     public static void main(String[] args) {
         int[][] accounts = {
             {1,2,3},
-            {3,2,1}
+            {3,2,1,5}
         };
-        System.out.println(wealthier(accounts));
+        System.out.println(maximumWealth(accounts));
     }
 
-    static int wealthier(int[][] accounts) {
+    static int maximumWealth(int[][] accounts) {
         int maxWealth = Integer.MIN_VALUE;
-
         for(int person = 0; person < accounts.length; person++) {
-            int accountSum = 0;
-            for(int account = 0; account < accounts[person].length; account++) {
-                accountSum += accounts[person][account];
+            int sumOfWealth = 0;
+            for(int account = 0; account < accounts[person].length; account++){
+                sumOfWealth += accounts[person][account];
             }
-            if(accountSum > maxWealth) {
-                maxWealth = accountSum;
-            }
+            if(sumOfWealth > maxWealth)
+                maxWealth = sumOfWealth;
         }
         return maxWealth;
     }
