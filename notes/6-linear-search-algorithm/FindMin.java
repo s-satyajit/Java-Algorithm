f75d44289aa8252adf68dc1a071ad9f8f15163af
Satyajit Samal <samalsatyajit668@gmail.com>
2024-06-26 08:23:59 +0530

    Modified the code

--- notes/6-linear-search-algorithm/FindMin.java
+++ notes/6-linear-search-algorithm/FindMin.java
@@ -1,6 +1,6 @@
 public class FindMin{
     public static void main(String[] args) {
-        int[] arr = {18, 12, -7, 3, 14,  28};
+        int[] arr = {18, 12, -7, 3, 14, 28};
         System.out.println(min(arr));
     }
 
